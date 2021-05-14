91a6b01d3408c8cc03e3622cf4527e064be9ade2
Toby <agentalliancela33@gmail.com>
2021-05-13 19:31:13 -0700

    node_life

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,84 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        email
+        username
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+
+export const SAVE_BOOK = gql`
+  mutation savedBook( $bookData: BookInput! ) {
+    savedBook(bookData: $bookData) {
+        _id
+        username
+        email
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook( $bookId: ID! ) {
+      removeBook(bookId: $bookId) {
+        _id
+        username
+        email
+        savedBooks {
+            bookId
+            authors
+            image
+            link
+            title
+            description
+        }
+      }
+  }
+`;
+
+// export const REMOVE_BOOK = gql`
+//   mutation removeBook($description: String!, $authors: String!, $bookId: String!, $image: String!, $link: String!, $title: String!) {
+//     removeBook(description: $description, authors: $authors, bookId: $bookId, image: $image, link: $link, title: $title) {
+//         _id
+//         username
+//         email
+//         saveBook{authors
+//           description
+//           bookId
+//           image
+//           link
+//           title }
+        
+    
+      
+//     }
+//   }
+// `;
+
+
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,79 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        // .populate('thoughts')
+        // .populate('friends');
+            console.log(userData)
+        return userData;
+      }
+      throw new AuthenticationError("Not logged in");
+    },
+  },
+
+  //   user async (parent, { username }) => {
+  //     return User.findOne({ username }).select("-__v -password");
+  //   },
+
+  //   users async () => {
+  //     return User.find().select("-__v -password");
+  //   }
+  // },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      const correctPw = await user.isCorrectPassword(password);
+      console.log(user);
+      // if (!user) {
+      //   throw new AuthenticationError("Not found!");
+
+      //   if (!pw) {
+      //     throw new AuthenticationError("Nope");
+      //   }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    
+    savedBook: async (parent, { bookData }, context) => {
+      console.log(context.user, context);
+      if (context.user) {
+        const updateUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: bookData } },
+          { new: true, runValidators: true }
+        );
+        return updateUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updateBook = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: {bookId} } },
+          { new: true, runValidators: true }
+        );
+        return updateBook;
+      }
+      throw new AuthenticationError("You must be loggedin!");
+    },
+  },
+};
+
+module.exports = resolvers;
